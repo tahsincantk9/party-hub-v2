a3dd30e8c4a479fff8ffa4aa60b1e31d6b533e4a
tahsincantk9 <tahsincantk9@gmail.com>
2026-05-30 00:40:42 +0300

    Update app.js

--- public/app.js
+++ public/app.js
@@ -10,13 +10,13 @@ import {
 
 /* FIREBASE CONFIG */
 const firebaseConfig = {
-  apiKey: "XXX",
-  authDomain: "XXX",
-  databaseURL: "https://YOUR-PROJECT-default-rtdb.firebaseio.com/",
-  projectId: "XXX",
-  storageBucket: "XXX",
-  messagingSenderId: "XXX",
-  appId: "XXX"
+  apiKey: "AIzaSyBZNpGv5Yk54JFB_5U6Qr6iNx2PaPrhIFo",
+  authDomain:  "party-hub-90183.firebaseapp.com",
+  databaseURL: "https://party-hub-90183-default-rtdb.europe-west1.firebasedatabase.app/",
+  projectId: "party-hub-90183",
+  storageBucket:  "party-hub-90183.firebasestorage.app",
+  messagingSenderId: "230836884321",
+  appId: "1:230836884321:web:81b3eb36d650c18d0d6b20"
 };
 
 const app = initializeApp(firebaseConfig);
