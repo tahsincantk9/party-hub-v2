9c70e0e838ef3dfaf294980dacc63355ba06b3b2
tahsincantk9 <tahsincantk9@gmail.com>
2026-05-29 21:45:39 +0300

    Update app.js

--- public/app.js
+++ public/app.js
@@ -14,9 +14,9 @@ const firebaseConfig = {
 
   apiKey: "AIzaSyBZNpGv5Yk54JFB_5U6Qr6iNx2PaPrhIFo",
   authDomain:  "party-hub-90183.firebaseapp.com",
-  databaseURL: "https://firebase.google.com/docs/web/setup#available-libraries" ,
+  databaseURL: "https://party-hub-90183-default-rtdb.europe-west1.firebasedatabase.app/" ,
   projectId: "party-hub-90183",
-  storageBucket: "party-hub-90183.firebasestorage.app",
+  storageBucket: "party-hub-90183.appspot.com",,
   messagingSenderId:  "230836884321",
   appId: "1:230836884321:web:81b3eb36d650c18d0d6b20"
 
