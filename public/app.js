92ec05beffaefcc056dd591dafc6e167ca1b2f84
tahsincantk9 <tahsincantk9@gmail.com>
2026-05-29 21:11:05 +0300

    Update app.js

--- public/app.js
+++ public/app.js
@@ -9,12 +9,38 @@ function joinRoom(){
     name = document.getElementById("name").value;
 
     if(!roomId || !name){
-        alert("Eksik bilgi");
+        alert("Bilgileri doldur");
         return;
     }
 
-    socket.emit("joinRoom", { roomId, name });
+    document.getElementById("login").style.display = "none";
+    document.getElementById("app").style.display = "block";
 
-    document.getElementById("status").innerText =
-        "Giriş yapıldı: " + roomId + " | " + name;
+    document.getElementById("roomText").innerText =
+        "🏠 Oda: " + roomId;
+
+    addPlayer(name);
+}
+
+function addPlayer(player){
+
+    const div = document.createElement("div");
+
+    div.innerText = "👤 " + player;
+
+    document.getElementById("players").appendChild(div);
+}
+
+function nextTurn(){
+
+    const players =
+        document.querySelectorAll("#players div");
+
+    if(players.length === 0) return;
+
+    const random =
+        Math.floor(Math.random() * players.length);
+
+    document.getElementById("turn").innerText =
+        "🎯 Sıra: " + players[random].innerText;
 }
